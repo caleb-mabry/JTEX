bca72f521e0dcd29978f35e0c3747176ba493c01
Caleb Mabry <caleb.mabry.15@cnu.edu>
2018-12-18 15:31:06 -0500

    Fixed the file to properly work for 0x03 hopefully

--- JTEXFormat.cs
+++ JTEXFormat.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Drawing;
+
+
+
 
 namespace JTEXFileFormat
 {
     class JTEXFormat
     {
+        public int x;
+        public int y;
 
         static void Main(string[] args)
         {
@@ -25,17 +31,46 @@ namespace JTEXFileFormat
                 int strideWidth = sr.ReadInt32();
                 int strideHeight = sr.ReadInt32();
                 sr.BaseStream.Position = fileDataOffset;
-                using (BinaryWriter br = new BinaryWriter(File.Open("Character.jtex", FileMode.Create)))
+                Bitmap newImage = new Bitmap(imageWidth, imageHeight);
+                using (BinaryWriter bw = new BinaryWriter(File.Open("JTEXFileData.txt", FileMode.Create)))
                 {
                     if (encodingIdentifier == 0x04) // 0X04=RGBA4444
                     {
-
+                        for (x = 0; x < newImage.Width; x++)
+                        {
+                            for (y = 0; y < newImage.Height; y++)
+                            {
+                                Color pixelColor = newImage.GetPixel(x, y);
+                                Color newColor = Color.FromArgb(sr.ReadBytes(4), sr.ReadBytes(4), sr.ReadBytes(8), sr.ReadBytes(8));
+                                newImage.SetPixel(x, y, newColor);
+                            }
+                        }
                     }
                     if (encodingIdentifier == 0x03) // 0x03=RGB888
                     {
+                        for (y = 0; y < newImage.Height; y++)
+                        {
+                            for (x = 0; x < newImage.Width; x++)
+                            {
+                                int R = sr.ReadByte();
+                                int G = sr.ReadByte();
+                                int B = sr.ReadByte();
+                                Color newColor = Color.FromArgb(R, G, B);
+                                newImage.SetPixel(x, y, newColor);
+                            }
+                        }
                     }
                     if (encodingIdentifier == 0x02) // 0X02=RGBA8888
                     {
+                        for (x = 0; x < newImage.Width; x++)
+                        {
+                            for (y = 0; y < newImage.Height; y++)
+                            {
+                                Color pixelColor = newImage.GetPixel(x, y);
+                                Color newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                                newImage.SetPixel(x, y, newColor);
+                            }
+                        }
                     }
                 }
                 

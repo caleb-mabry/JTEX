f17ea4702c7b18a09de6388236abacc82a8d4b91
Caleb Mabry <caleb.mabry.15@cnu.edu>
2018-12-17 19:54:25 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JTEXFileFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

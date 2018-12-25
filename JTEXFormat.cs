590f7862c485d17c3ac34952f5ef9eda2373a01a
Caleb Mabry <caleb.mabry.15@cnu.edu>
2018-12-25 13:44:03 -0500

    Updated the file to include what I should be working on figuring out

--- JTEXFormat.cs
+++ JTEXFormat.cs
@@ -5,8 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Drawing;
-
-
+// TODO: The files are still not being converted properly
 
 
 namespace JTEXFileFormat
